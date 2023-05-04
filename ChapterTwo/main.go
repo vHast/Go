1db5573cff000813a14483a0652d194625b597cd
vHast <fedvhast@gmail.com>
2023-05-04 23:03:50 +0300

    Formatting

--- ChapterTwo/main.go
+++ ChapterTwo/main.go
@@ -3,21 +3,21 @@ package main
 import (
 	"fmt"
 	"reflect"
-	"time"
 	"strconv"
+	"time"
 )
 
 func main() {
-	var num1 = 5				// type inferred
-	var num2 int				// *explicitly typed
+	var num1 = 5            // type inferred
+	var num2 int            // *explicitly typed
 	var rates float32 = 4.5 // declared as float32 and initialized
 
-	fmt.Println(num1,num2,rates);
+	fmt.Println(num1, num2, rates)
 
 	// Another way todeclare and initialize a variable is to use the short variable declaration (:=)
 
 	firstName := "Wei-Meng"
-	fmt.Println(firstName) // Will print Wei-Meng
+	fmt.Println(firstName)                 // Will print Wei-Meng
 	fmt.Println(reflect.TypeOf(firstName)) // Will print the type of firstName
 
 	// Variables declared with the (:=) operator can't be used outside function bodies
@@ -28,7 +28,7 @@ func main() {
 
 	fmt.Println(address)
 
-	quotation := 	`"Anyone who has ever made
+	quotation := `"Anyone who has ever made
 	a mistake has never tried
 	anything new."
 	-- Albert Einstein`
@@ -45,7 +45,7 @@ func main() {
 	// \n is a line feed or newline
 	// start is the target variable
 
-	fmt.Println(reflect.TypeOf(start)) // time.Time
+	fmt.Println(reflect.TypeOf(start))         // time.Time
 	fmt.Println(reflect.ValueOf(start).Kind()) // struct
 
 	// * Converting a variable's type
@@ -78,25 +78,25 @@ func main() {
 	// We can convert string values to specific types, such as Boolean, floating point numbers, integers, you may use the Parse functions
 
 	b, bErr := strconv.ParseBool("t")
-	fmt.Println(b) // true
-	fmt.Println(bErr) // <nil>
+	fmt.Println(b)           // true
+	fmt.Println(bErr)        // <nil>
 	fmt.Printf("%T\n", bErr) // bool
 
 	f, fErr := strconv.ParseFloat("3.1415", 64) //First value for ParseFloat is the value and then the specified bitsize for the float
 
-	fmt.Println(f) // 3.1415
-	fmt.Println(fErr) // <nil>
+	fmt.Println(f)        // 3.1415
+	fmt.Println(fErr)     // <nil>
 	fmt.Printf("%T\n", f) // float64
 
 	i, iErr := strconv.ParseInt("-18.56", 10, 64) // First value for ParseInt is the value, then the base and the bitsize for that int
 
-	fmt.Println(i) // 0
-	fmt.Println(iErr)			//ERROR: Parsing "18.56": invalid syntax, should be a float
-	fmt.Printf("%T\n", i)	//int64
+	fmt.Println(i)        // 0
+	fmt.Println(iErr)     //ERROR: Parsing "18.56": invalid syntax, should be a float
+	fmt.Printf("%T\n", i) //int64
 
 	u1, u1Err := strconv.ParseUint("18", 10, 64)
-	fmt.Println(u1) // 18
-	fmt.Println(u1Err) // <nil>
+	fmt.Println(u1)        // 18
+	fmt.Println(u1Err)     // <nil>
 	fmt.Printf("%T\n", u1) // uint64
 
 	// To convert between the various numeric data types like int and float, you can simply use the int(), float32() and float64() functions
@@ -111,14 +111,14 @@ func main() {
 	fmt.Printf("%T\n", number2) // float32
 	fmt.Printf("%T\n", number3) // float64
 	fmt.Printf("%T\n", number4) // float32
-	fmt.Printf("%T\n", number5) // int 
+	fmt.Printf("%T\n", number5) // int
 
 	// * INTERPOLATING STRINGS
 
 	queue := 5
 	nameQueue := "John"
 
-	// If you would like to concatenate you would do something like this 
+	// If you would like to concatenate you would do something like this
 
 	// * s := nameQueue + ", your queue number is:" + queue
 
@@ -134,6 +134,6 @@ func main() {
 
 	// The Sprintf() function formats a string baesd on the formatting verbs(such as %d and %s)
 
-	fmt.Println(s);
+	fmt.Println(s)
 
 }
